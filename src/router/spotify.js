260a55554a0247540d65b509fa5f5aaad5eee8ff
umer memon <117743102+umermemon4648@users.noreply.github.com>
2024-02-17 01:50:11 +0500

    updated

--- src/router/spotify.js
+++ src/router/spotify.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const spotify = require("../controllers/spotifyController");
 const isAuthenticated = require("../middleware/auth");
 
-router.route("/add").post(isAuthenticated, spotify.addPlaylist);
+router.route("/add").post(spotify.addPlaylist);
 router.route("/categories").get(spotify.fetchCategories);
 router.route("/playlist").post(spotify.getPlaylist);
 
